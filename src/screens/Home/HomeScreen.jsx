eb74df8cb889d8fc3ed9bacc9cbf9399ceed7cb1
¨Leonardo <leonardoantezana59@gmail.com>
2023-04-06 12:46:46 -0300

    dando estilos y cargando data

--- src/screens/Home/HomeScreen.jsx
+++ src/screens/Home/HomeScreen.jsx
@@ -1,10 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { useCallApi } from '../../hooks/useCallApi'
+import BASE_URL from '../../constants/Request'
+import { FlatList, StyleSheet } from 'react-native'
 import { ScreenCustom } from '../../components'
+import { CardCharacter } from '../../components'
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
+  
+  const { data: characters, isPending } = useCallApi(`${BASE_URL}/character`);
+  
+  const handleSelect = id => {
+    navigation.navigate('Details', {id})
+  }
+
+  const renderCharacter = ({item}) => (
+    <CardCharacter character={item} onSelected={handleSelect}/>
+  )
+
   return (
-    <ScreenCustom>
-      <Text>Home</Text>
+    <ScreenCustom style={styles.container}>
+      
+      <FlatList
+      data={characters.results}
+      renderItem={renderCharacter}
+      keyExtractor={item => item.id}
+      numColumns={2}
+      />
     </ScreenCustom>
   )
 }
@@ -12,5 +32,8 @@ const HomeScreen = () => {
 export default HomeScreen
 
 const styles = StyleSheet.create({
-  
+  container: {
+    paddingHorizontal: 20,
+    paddingBottom: 80,
+  },
 })
--- src/screens/Search/SearchScreen.jsx
+++ src/screens/Search/SearchScreen.jsx
@@ -1,16 +1,58 @@
-import { StyleSheet, Text} from 'react-native'
+import { useState, useRef } from 'react'
+import { StyleSheet, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard } from 'react-native'
 import { ScreenCustom } from '../../components'
+import { Ionicons } from '@expo/vector-icons';
+import Colors from '../../constants/Colors';
 
 const SearchScreen = () => {
+  const [valueSearch, setValueSearch] = useState(null)
+  const searchInput = useRef(null); 
+  
+  const onChangeInput = value => setValueSearch(value)
+
+  const handleOnPress = () => {
+    searchInput.current.focus();
+  }
+
+  const closeKeyboard = () => Keyboard.dismiss()
+
   return (
-    <ScreenCustom>
-      <Text>SearchScreen</Text>
-    </ScreenCustom>
+    <TouchableWithoutFeedback onPress={closeKeyboard}>
+      <ScreenCustom style={styles.screen}>
+        <TouchableOpacity style={styles.box} onPress={handleOnPress}>
+          <Ionicons name='search-outline' size={20} color='#fff'/>
+          <TextInput
+          ref={searchInput}
+          style={styles.text}
+          placeholder='Buscar personaje'
+          placeholderTextColor='#fff'
+          onChangeText={onChangeInput}
+          maxLength={20}
+          />
+        </TouchableOpacity>
+      </ScreenCustom>
+    </TouchableWithoutFeedback>
   )
 }
 
 export default SearchScreen
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    alignItems: 'center'
+  },
+  box: {
+    marginVertical: 20,
+    width: '80%',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 5,
+    backgroundColor: Colors.secondaryBlack,
+  },
+  text: {
+    color: '#fff',
+    marginLeft: 10,
+  }
 })
