86a4aafce563e586f5e3ae7fcfa9b8e51ccce8ee
¨Leonardo <leonardoantezana59@gmail.com>
2023-04-06 12:47:50 -0300

    cargando fuentes

--- App.js
+++ App.js
@@ -1,7 +1,27 @@
+import { useEffect } from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native';
+import { useFonts, RobotoCondensed_400Regular, RobotoCondensed_700Bold } from '@expo-google-fonts/roboto-condensed'
+import * as SplashScreen from 'expo-splash-screen'
 import MainNavigator from './src/navigator/MainNavigator';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    RobotoCondensed_400Regular,
+    RobotoCondensed_700Bold
+  })
+
+  useEffect(() => {
+    if(fontsLoaded){
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded])
+
+  if(!fontsLoaded){
+    return null
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <MainNavigator/>
