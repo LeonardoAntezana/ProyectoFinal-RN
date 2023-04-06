5f5195d45b685221135ee27d894929cbe78a564e
¨Leonardo <leonardoantezana59@gmail.com>
2023-04-06 19:17:28 -0300

    update barrel

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as CardCharacter } from './CardCharacter';
+export { default as DetailsCharacter } from './DetailsCharacter';
 export { default as ScreenCustom } from './ScreenCustom';
 export { default as TabIconCustom } from './TabIconCustom';
 export { default as UserIcon } from './UserIcon';
