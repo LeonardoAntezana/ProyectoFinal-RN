c1ced8197f00987df82db483724967da75dbb641
¨Leonardo <leonardoantezana59@gmail.com>
2023-04-12 15:40:08 -0300

    refactorizando codigo

--- src/components/DetailsCharacter.jsx
+++ src/components/DetailsCharacter.jsx
@@ -1,37 +1,28 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { addFavorite, deleteFavorite } from '../store/actions/favorites.action';
 import { StyleSheet, Text, ImageBackground, View, Pressable } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons';
 import Fonts from '../constants/Fonts';
 import Colors from '../constants/Colors';
 
-const DetailsCharacter = ({item}) => {
-  const {id, name, image, gender, species, origin, status, episode} = item;
-  
-  const dispatch = useDispatch();
-  const favorites = useSelector(state => state.favorites.favorites);
-
-  const favoriteExists = () => favorites.some(elem => elem.id === id);
-
-  const handleFav = (item) => favoriteExists() ? dispatch(deleteFavorite(item.id)) : dispatch(addFavorite(item));
+const DetailsCharacter = ({item, onSelect, exist}) => {
+  const {name, image, gender, species, originName, status, numEpisodes} = item;
 
   return (
     <>
       <Text style={styles.title}>{name}</Text>
       <ImageBackground source={{uri: image}} style={styles.image}>
-        <Pressable style={styles.fav} onPress={() => handleFav(item)}>
+        <Pressable style={styles.fav} onPress={() => onSelect(item)}>
             <MaterialIcons 
             name="favorite" 
             size={28} 
-            color={favoriteExists() ? 'red' : 'white'}/>
+            color={exist() ? 'red' : 'white'}/>
         </Pressable>
       </ImageBackground>
       <View style={styles.box}>
         <Text>Gender: {gender}</Text>
         <Text>Species: {species}</Text>
-        <Text>Origin: {origin.name}</Text>
+        <Text>Origin: {originName}</Text>
         <Text>Status: {status}</Text>
-        <Text>Episodes: {episode.length}</Text>
+        <Text>Episodes: {numEpisodes}</Text>
       </View>
     </>
   )
--- src/screens/Home/DetailsScreen.jsx
+++ src/screens/Home/DetailsScreen.jsx
@@ -1,14 +1,22 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { addFavorite, deleteFavorite } from '../../store/actions/favorites.action'
 import { StyleSheet } from 'react-native'
 import { ScreenCustom } from '../../components'
 import { DetailsCharacter } from '../../components';
 
 const DetailsScreen = ({route}) => {
-  
   const {params} = route;
+  
+  const dispatch = useDispatch();
+  const favorites = useSelector(state => state.favorites.favorites);
+
+  const favoriteExists = () => favorites.some(elem => elem.id === params.id);
+
+  const handleFav = (item) => favoriteExists() ? dispatch(deleteFavorite(item.id)) : dispatch(addFavorite(item))
 
   return (
     <ScreenCustom style={styles.screen}>
-      <DetailsCharacter item={params}/>
+      <DetailsCharacter item={params} onSelect={handleFav} exist={favoriteExists}/>
     </ScreenCustom>
   )
 }
