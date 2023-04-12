fcf7332eacf7cb34a68a5be2fb0426800794000b
¨Leonardo <leonardoantezana59@gmail.com>
2023-04-12 09:57:10 -0300

    despachando actions de favorites

--- src/components/DetailsCharacter.jsx
+++ src/components/DetailsCharacter.jsx
@@ -1,12 +1,31 @@
-import { StyleSheet, Text, Image, View } from 'react-native'
+import { useDispatch, useSelector } from 'react-redux';
+import { addFavorite, deleteFavorite } from '../store/actions/favorites.action';
+import { StyleSheet, Text, ImageBackground, View, Pressable } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons';
 import Fonts from '../constants/Fonts';
+import Colors from '../constants/Colors';
 
 const DetailsCharacter = ({item}) => {
   const {id, name, image, gender, species, origin, status, episode} = item;
+  
+  const dispatch = useDispatch();
+  const favorites = useSelector(state => state.favorites.favorites);
+
+  const favoriteExists = () => favorites.some(elem => elem.id === id);
+
+  const handleFav = (item) => favoriteExists() ? dispatch(deleteFavorite(item.id)) : dispatch(addFavorite(item));
+
   return (
     <>
       <Text style={styles.title}>{name}</Text>
-      <Image source={{uri: image}} style={styles.image}/>
+      <ImageBackground source={{uri: image}} style={styles.image}>
+        <Pressable style={styles.fav} onPress={() => handleFav(item)}>
+            <MaterialIcons 
+            name="favorite" 
+            size={28} 
+            color={favoriteExists() ? 'red' : 'white'}/>
+        </Pressable>
+      </ImageBackground>
       <View style={styles.box}>
         <Text>Gender: {gender}</Text>
         <Text>Species: {species}</Text>
@@ -30,10 +49,15 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     width: '100%',
     height: 250,
+    alignItems: 'flex-end',
   },
   box: {
     width: '100%',
     padding: 15,
     backgroundColor: '#fff',
+  },
+  fav: {
+    padding: 6,
+    backgroundColor: Colors.primary,
   },
 })
