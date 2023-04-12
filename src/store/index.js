a12adf7fa1b657293a74bb01f0fc9fd9b81e47fd
¨Leonardo <leonardoantezana59@gmail.com>
2023-04-12 15:42:10 -0300

    incorporando middleware a la store

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 import FavoritesReducer from "./reducers/favorites.reducer";
 
@@ -6,4 +7,4 @@ const RootReducer = combineReducers({
     favorites: FavoritesReducer,
 })
 
-export default createStore(RootReducer);
+export default createStore(RootReducer, applyMiddleware(thunk));
