f2cd0919797f64960f717c2c6e025c5ef1bdfa68
¨Leonardo <leonardoantezana59@gmail.com>
2023-04-14 16:41:12 -0300

    incorporando mas buscados

--- src/screens/Home/HomeScreen.jsx
+++ src/screens/Home/HomeScreen.jsx
@@ -1,12 +1,14 @@
 import { useCallApi } from '../../hooks/useCallApi'
 import BASE_URL from '../../constants/Request'
-import { FlatList, StyleSheet } from 'react-native'
-import { ScreenCustom } from '../../components'
+import { FlatList, StyleSheet, View } from 'react-native'
+import { ScreenCustom, Title } from '../../components'
 import { CardCharacter } from '../../components'
 
 const HomeScreen = ({navigation}) => {
-  
-  const { data: characters, isPending } = useCallApi(`${BASE_URL}/character`);
+
+  const { data: wanted } = useCallApi(`${BASE_URL}/character/1,2,3,4`);
+
+  const { data: characters, isPending } = useCallApi(`${BASE_URL}/character/?page=5`);
   
   const handleSelect = item => {
     const {id, name, image, gender, species, origin, status, episode} = item
@@ -17,14 +19,41 @@ const HomeScreen = ({navigation}) => {
     <CardCharacter character={item} onSelected={handleSelect}/>
   )
 
+  const renderWanted = ({item}) => (
+    <CardCharacter style={styles.charac} character={item} onSelected={handleSelect} showContent={false}/>
+  )
+
+  if(isPending){
+    return(
+      <ScreenCustom style={[styles.container, {justifyContent: 'center', alignItems: 'center'}]}>
+        <Title text='CARGANDO...'/>
+      </ScreenCustom>
+    )
+  }
+
   return (
     <ScreenCustom style={styles.container}>
       <FlatList
       data={characters.results}
       renderItem={renderCharacter}
       keyExtractor={item => item.id}
+      showsVerticalScrollIndicator={false}
+      ListHeaderComponent={() => {
+        return(
+        <View style={styles.wantedContainer}>
+        <Title text='Mas buscados'/>
+        <FlatList
+        data={wanted}
+        renderItem={renderWanted}
+        keyExtractor={item => item.id}
+        horizontal= {true}
+        showsHorizontalScrollIndicator={false}
+        />
+        <Title text='Personajes' style={{marginBottom: 0}}/>
+        </View>
+        )}}
       numColumns={2}
-      />
+      />      
     </ScreenCustom>
   )
 }
@@ -33,7 +62,17 @@ export default HomeScreen
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 20,
+    paddingHorizontal: 15,
     paddingBottom: 80,
   },
+  charac: {
+    margin: 0,
+    marginRight: 10,
+    width: 250,
+    height: 150,
+  },
+  wantedContainer: {
+    marginHorizontal: 7,
+    marginBottom: 20,
+  }
 })
