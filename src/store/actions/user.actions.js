48adde682e4e79e02484b8a5f5198a3d4a186a82
¨Leonardo <leonardoantezana59@gmail.com>
2023-04-14 11:24:00 -0300

    incorporando action de login

--- src/store/actions/user.actions.js
+++ src/store/actions/user.actions.js
@@ -1,7 +1,8 @@
 import { Alert } from "react-native";
-import { AUTH_URL } from "../../../database"; 
+import { AUTH_URL, SIGN_IN_URL } from "../../../database"; 
 
 export const SIGN_UP = 'SIGN_UP';
+export const SIGN_IN = 'SIGN_IN';
 
 export const signUp = (email, password) => {
     return async dispatch => {
@@ -42,4 +43,49 @@ export const signUp = (email, password) => {
             token: data.idToken,
         })
     } 
+}
+
+export const signIn = (email, password) => {
+    return async dispatch => {
+        const res = await fetch(SIGN_IN_URL, {
+            method: 'POST',
+            header: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email,
+                password
+            }),
+        });
+
+        if(!res.ok){
+            const dataError = await res.json();
+            const errorID = dataError.error.message;
+
+            let msj = 'Error al registrarse'
+
+            if(errorID === 'EMAIL_NOT_FOUND'){
+                msj= 'El email no se encuentra registrado';
+                Alert.alert(msj)
+            }
+            if(errorID === 'INVALID_PASSWORD'){
+                msj='La contraseña es invalida'
+                Alert.alert(msj)
+            }
+            else{
+                Alert.alert(msj)
+            }
+            
+            return
+        }
+
+        const data = await res.json();
+
+        dispatch({
+            type: SIGN_IN,
+            name: data.email,
+            userId: data.localId,
+            token: data.idToken,
+        })
+    }
 }
--- src/store/reducers/user.reducer.js
+++ src/store/reducers/user.reducer.js
@@ -1,4 +1,4 @@
-import { SIGN_UP } from "../actions/user.actions"
+import { SIGN_UP, SIGN_IN } from "../actions/user.actions"
 
 const initialState = {
     name: '',
@@ -9,6 +9,12 @@ const initialState = {
 const AuthReducer = (state = initialState, action) => {
     switch(action.type){
         case SIGN_UP:
+            return {
+                name: action.name,
+                userID: action.userId,
+                tokenID: action.token
+            }
+        case SIGN_IN:
             return {
                 name: action.name,
                 userID: action.userId,
