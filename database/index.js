f5a6784ff573afd18f2653c1b4f5ebce4b27ad43
¨Leonardo <leonardoantezana59@gmail.com>
2023-04-21 10:18:47 -0300

    sacando de .gitignore la carpeta database

---
+++ database/index.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://rickymortyapp-77869-default-rtdb.firebaseio.com/';
+
+export const AUTH_URL = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAgXcWhE0VG9X3ski6xz0I7TDDd1YZStHk'
+
+export const SIGN_IN_URL = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAgXcWhE0VG9X3ski6xz0I7TDDd1YZStHk'
